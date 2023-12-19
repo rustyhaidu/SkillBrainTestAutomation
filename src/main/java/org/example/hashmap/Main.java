5dee7516a4a8cfe6703541e62790d06c51f87c9a
Claudiu Haidu <rustyhaidu@yahoo.com>
2023-12-19 21:16:19 +0200

    hashmap si hashset

--- src/main/java/org/example/hashmap/Main.java
+++ src/main/java/org/example/hashmap/Main.java
@@ -1,7 +1,9 @@
 package org.example.hashmap;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +11,22 @@ public class Main {
 
         map.put(1, "Pisica");
         map.put(9, "Dog");
+        map.put(9, "Cangur");
 
         System.out.println(map.get(9));
+
+        Set<String> set = new HashSet<>();
+
+        set.add("Hamster");
+        set.add("Bird");
+        set.add("Hamster");
+
+        for(String crt : set){
+            System.out.println(crt);
+        }
+
+        for(Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
 }
