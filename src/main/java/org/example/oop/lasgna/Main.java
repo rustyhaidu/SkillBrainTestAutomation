8da7146515e99d824628df3acdf42332c7d99bb7
Claudiu Haidu <rustyhaidu@yahoo.com>
2023-12-21 19:42:56 +0200

    annalysinfiltration impl

---
+++ src/main/java/org/example/oop/lasgna/Main.java
@@ -0,0 +1,2 @@
+package org.example.oop.lasgna;public class Main {
+}
---
+++ src/test/java/org/example/AnalynsInfiltrationTest.java
@@ -0,0 +1,11 @@
+package org.example.oop.annalynsinfiltration;
+
+import org.junit.jupiter.api.Test;
+
+public class AnalynsInfiltrationTest {
+
+    @Test
+    public void canPrisonerCanBeFreed(){
+
+    }
+}
